8a710426fdc44b3a15c8af8adb89a875ca12bb24
flaviengibs <149933385+flaviengibs@users.noreply.github.com>
2024-06-24 16:59:40 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,22 @@ const keepAlive = require('./server'); // Importer le fichier server.js
 
 const client = new Client(intents:8);
 
+let badWords["putain", "merde", "chiant", "con", "salope", "salopard", "pute", "fdp", "salaud", "ta mère la", "porn","chiasse", "bite", "zizi", "encullé", "enculleur", "couilles"]
+
 client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
+client.on('messageCreate', message => {
+    const content = message.toLowerCase;
+    if (bannedWords.some(word => content.includes(word))) {
+        try {
+            await message.delete();
+            await message.channel.send(`Message supprimé car il contenait un mot interdit.`);
+        } catch (error) {
+            console.error('Erreur lors de la suppression du message:', error);
+        }
+        }
+}); 
 
-
-keepAlive(); // Garder le serveur actif
-client.login(process.env.DISCORD_TOKEN);
+client.login(MTI1NDUwMDYxNTgzNzU4MTQxNQ.G4MtI6.iCltycI67Evwv0Mp283bphLIde7pCYT8hCSUS8);
