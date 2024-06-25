e00e084d6ef9f53a4ca4d218413f804b51554901
flaviengibs <149933385+flaviengibs@users.noreply.github.com>
2024-06-25 08:03:15 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ function escapeRegExp(string) {
 const escapedBannedWords = bannedWords.map(escapeRegExp);
 
 function containsExactWord(message, wordList) {
-  return bannedWords.some(word => {
+  return wordList.some(word => {
     const regex = new RegExp(`\\b${word}\\b`, 'i'); // 'i' pour insensible à la casse
     return regex.test(message.content);
   });
